1d973c3726196df878169fa654f771508c18d533
Adrian-George Bostan <adrg@epistack.com>
2022-04-12 16:45:53 +0300

    Update twoaxis example

--- examples/twoaxis/main.go
+++ examples/twoaxis/main.go
@@ -11,23 +11,30 @@ import (
 )
 
 func main() {
+	var timestamps []float64
+	for i := 0; i < 5; i++ {
+		date := time.Date(2000+i, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
+		timestamps = append(timestamps, float64(date.UnixNano()))
+	}
+
 	// Create chart component.
 	chart := &unichart.Chart{
 		XAxis: unichart.XAxis{
-			TickPosition: unichart.TickPositionBetweenTicks,
+			TickPosition: unichart.TickPositionUnderTick,
 			ValueFormatter: func(v interface{}) string {
-				typedDate := time.Unix(0, int64(v.(float64)))
-				return fmt.Sprintf("%d-%d\n%d", typedDate.Month(), typedDate.Day(), typedDate.Year())
+				d := time.Unix(0, int64(v.(float64)))
+				fmt.Println(int64(v.(float64)), d)
+				return fmt.Sprintf("%02d-%02d\n%d", d.Month(), d.Day(), d.Year())
 			},
 		},
 		Series: []dataset.Series{
 			dataset.ContinuousSeries{
-				XValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+				XValues: timestamps,
 				YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
 			},
 			dataset.ContinuousSeries{
 				YAxis:   dataset.YAxisSecondary,
-				XValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+				XValues: timestamps,
 				YValues: []float64{50.0, 40.0, 30.0, 20.0, 10.0},
 			},
 		},
