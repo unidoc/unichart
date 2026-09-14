57120b0379ff5f01e24de2a3a6cd00dd42ec1400
Anna Novikova <demonplus@gmail.com>
2026-09-14 14:57:37 +0200

    Updated minimum go version to 1.25 and UniPDF to v5.1.0.

--- examples/progress_bar/main.go
+++ examples/progress_bar/main.go
@@ -8,9 +8,9 @@ import (
 	"github.com/unidoc/unichart"
 	"github.com/unidoc/unichart/examples"
 	"github.com/unidoc/unichart/render"
-	"github.com/unidoc/unipdf/v4/common/license"
-	"github.com/unidoc/unipdf/v4/creator"
-	"github.com/unidoc/unipdf/v4/model"
+	"github.com/unidoc/unipdf/v5/common/license"
+	"github.com/unidoc/unipdf/v5/creator"
+	"github.com/unidoc/unipdf/v5/model"
 )
 
 func init() {
